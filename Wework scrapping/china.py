7dada41546f86bb11b1e3e084a9341a3e6a20136
PooH <48609388+welcome0605@users.noreply.github.com>
2019-09-22 14:48:19 +0900

    Wework scrapping
    Wework scrapping

--- Wework scrapping/china.py
+++ Wework scrapping/china.py
@@ -42,92 +42,127 @@ def add_csv_head():
 def add_csv_row(city, titles, po, dd, hd):
     with open(output_file, 'a', newline='', encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
+        # writer.writerow(['city', 'name', 'po1', 'po2', 'po3', 'po4', 'po5', 'po6', 'po7', 'po8', 'po9', 'po10', 'dd1', 'dd2', 'dd3', 'dd4', 'dd5', 'dd6', 'dd7', 'dd8', 'dd9', 'dd10', 'hd1', 'hd2', 'hd3', 'hd4', 'hd5', 'hd6', 'hd7', 'hd8', 'hd9', 'hd10'])
+        # for i in range(0, len(po)):       
+        # writer.writerows([city, title, po[0], po[1], po[2], po[3], po[4], po[5], po[6], po[7], po[8], po[9], dd[0], dd[1], dd[2], dd[3], dd[4], dd[5], dd[6], dd[7], dd[8], dd[9], hd[0], hd[1], hd[2], hd[3], hd[4], hd[5], hd[6], hd[7], hd[8], hd[9]])
+        # titles = content.find_elements_by_class_name('building-card__address')
+        # for content in contents:
+            # names.append(content)
+        # print(len(po), len(dd), len(hd))
+        # for i in range(0, len(po)):  
+        #     print(po[i])     
+        # print(po)
+        # print('\n')
+        # print(dd)
+        # print('\n')
+        # print(hd)
+        # for title in titles:
+        #     print(title.text)
+        # print('\n')
+        title = ['189 Yunxia Lu Oceanwide Fortune Center, 7/F., JIANGHAN Wuhan 430072', 'No.465 Guan Shan Da Dao, Hongshan Qu Wuhan Shi, Hubei Sheng Wuhan 430073']
         i = 0
-        for title in titles:
-            temp = []
-            temp.append(city)
-            # titles = content.find_elements_by_class_name('building-card__address')
-            temp.append(title)
-            j = 0
-            while j < 10:
-                temp.append(po[i + j * len(titles)])
-                j += 1
-            j = 0
-            while j < 10:
-                temp.append(dd[i + j * len(titles)])
-                j += 1
-            j = 0
-            while j < 10:
-                temp.append(hd[i + j * len(titles)])
-                j += 1
+        temp = []
+        temp.append(city)
+        temp.append(title[0])
 
-            i += 1
-            writer.writerow(temp)
+        j = 0
+        while j < 4:
+            temp.append(po[j])
+            j += 1
+        j = 5
+        while j <= 15:
+            temp.append(po[j])
+            j += 2
+
+        j = 0
+        while j < 4:
+            temp.append(dd[j])
+            j += 1
+        j = 5
+        while j <= 15:
+            temp.append(dd[j])
+            j += 2
+
+        j = 0
+        while j < 4:
+            temp.append(hd[j])
+            j += 1
+        j = 5
+        while j <= 15:
+            temp.append(hd[j])
+            j += 2
+
+        writer.writerow(temp)
+
+        temp = []
+        temp.append(city)
+        temp.append(title[1])
+        j = 0
+        while j < 3:
+            temp.append('n/a')
+            j += 1
+        j = 4
+        while j <= 16:
+            temp.append(po[j])
+            j += 2
+
+        j = 0
+        while j < 3:
+            temp.append('n/a')
+            j += 1
+        j = 4
+        while j <= 16:
+            temp.append(dd[j])
+            j += 2
+
+        j = 0
+        while j < 3:
+            temp.append('n/a')
+            j += 1
+        j = 4
+        while j <= 16:
+            temp.append(hd[j])
+            j += 2
+
+        writer.writerow(temp)
+
+
+            # j = 0
+            # while j < 10:
+            #     if j < 4:
+            #         temp.append()
+            #     temp.append(po[i + j * 2])
+            #     j += 1
+            # j = 0
+            # while j < 10:
+            #     temp.append(dd[i + j * 2])
+            #     j += 1
+            # j = 0
+            # while j < 10:
+            #     temp.append(hd[i + j * 2])
+            #     j += 1
+
+            # i += 1
+            
 
 def pull_data(base_url, city):
     po = []
     dd = []
     hd = []
-    sample = []
-# ray-text--body building-card__address
-    link = base_url + city + '?capacity=' + str('1')
-    driver.get(link)
-    contents = driver.find_elements_by_class_name('ray-card__content')
-    send_contents = contents
-    for content in contents:
-        titles = content.find_elements_by_class_name('building-card__address')
-        for title in titles:     
-            sample.append(title.text)
-
-        prices = content.find_elements_by_class_name('building-card__text-pricing')
-        for price in prices:
-            # names.append(title)
-            targets = price.find_elements_by_tag_name('div')
-            # print(len())
-            if len(targets) == 4:
-                j = 0
-                for target in targets:
-                    j += 1
-                    if j == 1:
-                        continue
-                    if j == 2:
-                        for real in target.find_elements_by_class_name('office-pricing'):
-                            po.append(real.text.replace('\n', ' ').replace(" ", ""))
-                            # print(real.text.replace('\n', ' ').replace(" ", ""))
-                    if j == 3:
-                        for real in target.find_elements_by_class_name('dedicated-desk-pricing'):
-                            dd.append(real.text.replace('\n', ' ').replace(" ", ""))
-                            # print(real.text.replace('\n', ' ').replace(" ", ""))
-                    if j == 4:
-                        for real in target.find_elements_by_class_name('hot-desk-pricing'):
-                            hd.append(real.text.replace('\n', ' ').replace(" ", ""))
-                            # print(real.text.replace('\n', ' ').replace(" ", ""))
-
-            else:
-                po.append('n/a')
-                dd.append('n/a')
-                hd.append('n/a')
-
-    # print(sample)
-
-    for i in range(2, 11):
-        t_po = []
-        t_dd = []
-        t_hd = []
+    for i in range(1,11):
         names = []
         link = base_url + city + '?capacity=' + str(i)
         driver.get(link)
         contents = driver.find_elements_by_class_name('ray-card__content')
-        for content in contents:
-            titles = content.find_elements_by_class_name('building-card__address')
-            for title in titles:
-                names.append(title.text)
 
+        for content in contents:
+
+            # content.find_elements_by_class_name('building-card__text-pricing')
             prices = content.find_elements_by_class_name('building-card__text-pricing')
             for price in prices:
                 # names.append(title)
                 targets = price.find_elements_by_tag_name('div')
-                # print(len(targets))
+                # print(len())
                 if len(targets) == 4:
                     j = 0
                     for target in targets:
@@ -136,48 +171,28 @@ def pull_data(base_url, city):
                             continue
                         if j == 2:
                             for real in target.find_elements_by_class_name('office-pricing'):
-                                t_po.append(real.text.replace('\n', ' ').replace(" ", ""))
+                                po.append(real.text.replace('\n', ' ').replace(" ", ""))
                                 # print(real.text.replace('\n', ' ').replace(" ", ""))
                         if j == 3:
                             for real in target.find_elements_by_class_name('dedicated-desk-pricing'):
-                                t_dd.append(real.text.replace('\n', ' ').replace(" ", ""))
+                                dd.append(real.text.replace('\n', ' ').replace(" ", ""))
                                 # print(real.text.replace('\n', ' ').replace(" ", ""))
                         if j == 4:
                             for real in target.find_elements_by_class_name('hot-desk-pricing'):
-                                t_hd.append(real.text.replace('\n', ' ').replace(" ", ""))
+                                hd.append(real.text.replace('\n', ' ').replace(" ", ""))
                                 # print(real.text.replace('\n', ' ').replace(" ", ""))
 
                 else:
-                    t_po.append('n/a')
-                    t_dd.append('n/a')
-                    t_hd.append('n/a')
-        # print('\n')
-        # print(names)
-
-        k = 0
-        for j in range(0, len(sample)):
-            if sample[j] in names:
-                # print("yes")
-                po.append(t_po[k])
-                dd.append(t_dd[k])
-                hd.append(t_hd[k])
-                k += 1
-            else:
-                # print("no")
-                po.append('n/a')
-                dd.append('n/a')
-                hd.append('n/a')
-        # print(po)
-        # print(dd)
-        # print(hd)
-
-    add_csv_row(city, sample, po, dd, hd)
+                    po.append('n/a')
+                    dd.append('n/a')
+                    hd.append('n/a')
+    add_csv_row(city, driver.find_elements_by_class_name('building-card__address'), po, dd, hd)
 
 driver = setUpChrome()
 add_csv_head()
 
 
-urls = ['wuhan', 'shenzhen', 'suzhou', 'hong-kong', 'shanghai--31']
+urls = ['wuhan']
 
 i = 0
 while i < 1:
@@ -187,5 +202,4 @@ while i < 1:
     i += 1
 
 print("done")
-
 # , 'shenzhen', 'suzhou', 'hong-kong', 'shanghai--31'
